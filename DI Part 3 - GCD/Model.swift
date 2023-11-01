2cb9d65162b636eb026c47acd23b3573a992bda4
Red Wang <redxoxored@gmail.com>
2023-11-01 11:34:28 +0800

    feat: Add data model to decode

--- DI Part 3 - GCD/Model.swift
+++ DI Part 3 - GCD/Model.swift
@@ -5,4 +5,36 @@
 //  Created by Red Wang on 2023/11/1.
 //
 
-import Foundation
+import UIKit
+
+struct Root: Codable {
+    let result: DataResult
+}
+
+struct DataResult: Codable {
+    let limit: Int
+    let offset: Int
+    let count: Int
+    let sort: String
+    let results: [ResultItem]
+}
+
+struct ResultItem: Codable {
+    let id: Int
+    let location: String
+    let district: String
+    let address: String
+    let longitude: String
+    let latitude: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case location
+        case district
+        case address
+        case longitude
+        case latitude
+    }
+        
+}
+
